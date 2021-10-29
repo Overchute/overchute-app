ae523c24e25a4fe335bf0c1a7fdd3492a892377a
FerMartz <fernandomvc@gmail.com>
2021-10-29 08:33:09 -0500

    working on homepage

--- frontend/views/HomePageView/index.jsx
+++ frontend/views/HomePageView/index.jsx
@@ -23,110 +23,126 @@ function HomePageView() {
   // const { actor, principal } = useAuth()
   // console.log("home", "actor :", actor, "principal :", principal)
   return (
-    <Container maxWidth="lg">
-      <Paper elevation="3">
-        <Box
-          margin="1rem 0 0 0"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-        >
-          <SearchCrowdsale />
-          <Box
-            maxWidth={{ xs: "95%", sm: "85%", md: "65%", lg: "55%", xl: "55%" }}
-            sx={{ textAlign: "center" }}
-            marginBottom="4rem"
-          >
-            <Typography variant="h5" gutterBottom style={{ margin: "2rem 0" }}>
-              A decentralized smart-contract application for crowdfunding the
-              release of intellectual property under open licences.
-            </Typography>
-          </Box>
-        </Box>
-        <Grid container spacing={6}>
-          <Grid item xs={12} sm={12} md={6} lg={6}>
+    <Container>
+      <Grid container spacing={10}>
+        <Grid item xs={12}>
+          <Paper elevation="3">
             <Box
+              margin="3rem 0"
               display="flex"
-              alignItems="center"
               flexDirection="column"
-              justifyContent="center"
-              padding="2rem"
-            >
-              <Typography variant="h4" sx={{ marginBottom: "1rem" }}>
-                Creators
-              </Typography>
-              <Typography variant="h6" gutterBottom>
-                build for the world to use
-              </Typography>
-              <img
-                src={creatorsScene}
-                width="85%"
-                style={{ padding: "2rem" }}
-              />
-              <Divider
-                variant="middle"
-                sx={{ borderColor: blueGrey[700], marginBottom: "1rem" }}
-              />
-              <Typography
-                variant="h6"
-                style={{ marginBottom: "1rem" }}
-                textAlign="center"
-              >
-                Monetize your work without having to enforce restrictive
-                licences.
-              </Typography>
-              <Divider light />
-              <Typography
-                variant="h6"
-                style={{ marginBottom: "1rem" }}
-                textAlign="center"
-              >
-                Get rewarded for producing public goods.
-              </Typography>
-            </Box>
-            <Box textAlign={"center"}></Box>
-          </Grid>
-          <Grid item xs={12} sm={12} md={6} lg={6}>
-            <Box
-              display="flex"
               alignItems="center"
-              flexDirection="column"
-              justifyContent="center"
-              padding="2rem"
             >
-              <Typography variant="h4" sx={{ marginBottom: "1rem" }}>
-                Communities
-              </Typography>
-              <Typography variant="h6" gutterBottom>
-                mobilize your collective buying power
-              </Typography>
-              <img
-                src={communitiesScene}
-                width="85%"
-                style={{ padding: "2rem" }}
-              />
-              <Divider variant="middle" />
-              <Typography
-                variant="h6"
-                style={{ marginBottom: "1rem" }}
-                textAlign="center"
+              <SearchCrowdsale />
+              <Box
+                maxWidth={{
+                  xs: "95%",
+                  sm: "85%",
+                  md: "65%",
+                  lg: "55%",
+                  xl: "55%",
+                }}
+                sx={{ textAlign: "center" }}
+                margin="3rem 0"
               >
-                Club together to release the products you need. You only pay if
-                others do too.
-              </Typography>
-              <Divider light />
-              <Typography
-                variant="h6"
-                style={{ marginBottom: "1rem" }}
-                textAlign="center"
-              >
-                Know what you’re getting up-front.
-              </Typography>
+                <Typography
+                  variant="h5"
+                  gutterBottom
+                  style={{ margin: "2rem 0" }}
+                >
+                  A decentralized smart-contract application for crowdfunding
+                  the release of intellectual property under open licences.
+                </Typography>
+              </Box>
             </Box>
-          </Grid>
+          </Paper>
         </Grid>
-        <Box height={"2000px"}></Box>
-      </Paper>
+        <Grid item xs={12}>
+          <Paper elevation="3">
+            <Box margin="3rem 0">
+              <Grid container spacing={6}>
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    flexDirection="column"
+                    justifyContent="center"
+                    padding="2rem"
+                  >
+                    <Typography variant="h4" sx={{ marginBottom: "1rem" }}>
+                      Creators
+                    </Typography>
+                    <Typography variant="h6" gutterBottom>
+                      build for the world to use
+                    </Typography>
+                    <img
+                      src={creatorsScene}
+                      width="85%"
+                      style={{ padding: "2rem" }}
+                    />
+                    <Divider sx={{ width: "100%", margin: "1rem 0" }} />
+                    <Typography
+                      variant="h6"
+                      style={{ marginBottom: "1rem" }}
+                      textAlign="center"
+                    >
+                      Monetize your work without having to enforce restrictive
+                      licences.
+                    </Typography>
+                    <Divider sx={{ width: "100%", margin: "1rem 0" }} />
+                    <Typography
+                      variant="h6"
+                      style={{ marginBottom: "1rem" }}
+                      textAlign="center"
+                    >
+                      Get rewarded for producing public goods.
+                    </Typography>
+                  </Box>
+                  <Box textAlign={"center"}></Box>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    flexDirection="column"
+                    justifyContent="center"
+                    padding="2rem"
+                  >
+                    <Typography variant="h4" sx={{ marginBottom: "1rem" }}>
+                      Communities
+                    </Typography>
+                    <Typography variant="h6" gutterBottom>
+                      mobilize your collective buying power
+                    </Typography>
+                    <img
+                      src={communitiesScene}
+                      width="85%"
+                      style={{ padding: "2rem" }}
+                    />
+                    <Divider sx={{ width: "100%", margin: "1rem 0" }} />
+                    <Typography
+                      variant="h6"
+                      style={{ marginBottom: "1rem" }}
+                      textAlign="center"
+                    >
+                      Club together to release the products you need. You only
+                      pay if others do too.
+                    </Typography>
+                    <Divider sx={{ width: "100%", margin: "1rem 0" }} />
+                    <Typography
+                      variant="h6"
+                      style={{ marginBottom: "1rem" }}
+                      textAlign="center"
+                    >
+                      Know what you’re getting up-front.
+                    </Typography>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
     </Container>
   )
 }
